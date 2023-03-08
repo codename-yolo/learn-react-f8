20b1f9912b677762ee8c2d4179637804bea4b010
cuongdm <cuongdm1999@gmail.com>
2023-03-08 23:44:30 +0700

    learn: gird css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from '@components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Header />
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/header" element={<Header />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Home.scss';
 
 const Home = () => {
   const say = (text: any) => {
     return text;
   };
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="grid-container">
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 2</div>
+      <div className="grid-item">Item 3</div>
+      <div className="grid-item">Item 4</div>
+      <div className="grid-item">Item 5</div>
     </div>
   );
 };
